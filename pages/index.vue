ba3b88df5d08fb995c524060cf9832a3ae7888e9
ebouther <ebouther@student.42.fr>
2021-07-09 15:02:28 +0200

    chore: UI - upgrade tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="md:pt-8 text-center">
       <div class="hidden md:block font-bold text-5xl font-mono text-gray-700">
-        <h1> <span class="text-orange-500"> Un Goût</span> <span class="text-gray-800">de  Liberté</span> </h1>
+        <h1> <span class="text-yellow-500"> Un Goût</span> <span class="text-gray-800">de  Liberté</span> </h1>
       </div>
       <div class="md:grid md:grid-cols-2 items-center md:pt-4 lg: xl:space-x-10 xl:mx-48">
         <div class="mx-auto self-center px-3 max-w-md">
@@ -12,7 +12,7 @@
         <div class="p-4 self-center">
           <!-- <div class="font-bold text-3xl font-mono text-gray-700">
             <h1>
-            <span class="text-orange-500"> Un Goût</span> <span class="text-gray-800">de  Liberté</span>
+            <span class="text-yellow-500"> Un Goût</span> <span class="text-gray-800">de  Liberté</span>
             </h1>
           </div> -->
           <div class="text-3xl font-light font-mono text-gray-700">
@@ -22,7 +22,7 @@
           </div>
           <br/>
           <br/>
-          <div class="text-orange-500 font-semibold text-2xl text-grey-800 font-mono">
+          <div class="text-yellow-500 font-semibold text-2xl text-grey-800 font-mono">
             <h2>
               <span class="">Pâtisserie</span>
               <br/>
@@ -49,7 +49,7 @@
 
     <div class="max-w-screen-lg mx-auto">
       <div class="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
-        <div v-for="p in products" :key="p.id" class="border rounded-lg bg-gray-100 shadow-lg hover:shadow-md focus:shadow-none hover:border-orange-500 hover:border-8">
+        <div v-for="p in products" :key="p.id" class="border rounded-lg bg-gray-100 shadow-lg hover:shadow-md focus:shadow-none hover:border-yellow-500 hover:border-8">
           <nuxt-link :to="`/products/${p.id}`">
               <div class=" rounded-t-lg bg-white">
                 <img class="object-cover h-48 w-full rounded-t-lg" :src="p.images[0]" :alt="p.name">
@@ -68,7 +68,12 @@
 <script>
 import cart from '../components/icons/cart.vue'
 import { mapState, mapGetters } from 'vuex'
-
+/*
+pages:
+  - Accueil
+  - Réalisations
+  - Contact
+*/
 export default {
   auth: false,
   components: { cart },
