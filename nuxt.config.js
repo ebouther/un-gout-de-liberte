b8e59b8e7bb2d11e3abce6e19151cf3125712209
ebouther <ebouther@student.42.fr>
2021-07-09 19:57:37 +0200

    fix: SEO - homepage meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,16 +18,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'title',
-        name: 'title',
-        content: 'Un Goût de Liberté'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description
-      }
     ],
     htmlAttrs: {
       lang: 'fr'
