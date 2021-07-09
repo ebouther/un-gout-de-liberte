392d633424b0b1124ecaf5be2f38d1efe75ea8b1
ebouther <ebouther@student.42.fr>
2021-07-09 17:41:36 +0200

    fix: vuex - state reactivity

--- pages/cart.vue
+++ pages/cart.vue
@@ -55,11 +55,11 @@ export default {
     ...mapState({
       items: (state) => state.cart.items,
       totalPrice: (state) => {
-        if (!Object.keys(state.cart.items).length) return 0
+        if (!Object.keys(state.cart.items).length) return 0;
 
         return Object.values(state.cart.items)
-                .reduce((acc, i) => acc + i.price.amount * i.quantity, 0)
-      }
+                .reduce((acc, i) => acc + i.price.amount * i.quantity, 0);
+      },
     }),
   },
 
