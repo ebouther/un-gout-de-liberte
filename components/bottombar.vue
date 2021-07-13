97279a6a03279d7124ea0aa1b0cf419bc1054211
ebouther <ebouther@student.42.fr>
2021-07-13 15:11:59 +0200

    fix: UI bottom bar icons

--- components/bottombar.vue
+++ components/bottombar.vue
@@ -1,58 +1,62 @@
 <template>
-<div class="bg-white shadow-lg rounded-2xl">
-  <div class="flex">
+  <div class="bg-white shadow-lg rounded-t-2xl">
+    <div class="flex">
       <div class="flex-1 group">
-          <a href="/" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
-              <span class="block px-1 pt-1 pb-1">
-                  <i class="far fa-home text-2xl pt-1 mb-1 block"></i>
-                  <!-- <span class="block text-xs pb-2">Accueil</span> -->
-                  <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
-              </span>
-          </a>
+        <a href="/" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
+          <span class="block px-1 pt-1 pb-1">
+            <Home class="text-2xl pt-1 mb-1 block"/>
+            <!-- <span class="block text-xs pb-2">Accueil</span> -->
+            <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
+          </span>
+        </a>
       </div>
       <div class="flex-1 group">
-          <a href="presentation" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
-              <span class="block px-1 pt-1 pb-1">
-                  <i class="far fa-address-card text-2xl pt-1 mb-1 block"></i>
-                  <!-- <span class="block text-xs pb-2">Présentation</span> -->
-                  <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
-              </span>
-          </a>
+        <a href="presentation" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
+          <span class="block px-1 pt-1 pb-1">
+            <About class="text-2xl pt-1 mb-1 block"/>
+            <!-- <span class="block text-xs pb-2">Présentation</span> -->
+            <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
+          </span>
+        </a>
       </div>
       <div class="flex-1 group">
-          <a href="produits" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
-              <span class="block px-1 pt-1 pb-1">
-                  <i class="far fa-cookie text-2xl pt-1 mb-1 block"></i>
-                  <!-- <span class="block text-xs pb-2">Réalisations</span> -->
-                  <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
-              </span>
-          </a>
+        <a href="produits" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
+          <span class="block px-1 pt-1 pb-1">
+            <Biscuit class="text-2xl pt-1 mb-1 block"/>
+            <!-- <span class="block text-xs pb-2">Réalisations</span> -->
+            <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
+          </span>
+        </a>
       </div>
       <div class="flex-1 group">
-          <a href="panier" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
-              <span class="block px-1 pt-1 pb-1">
-                  <i class="far fa-shopping-cart text-2xl pt-1 mb-1 block"></i>
-                  <!-- <span class="block text-xs pb-2">Panier</span> -->
-                  <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
-              </span>
-          </a>
+        <a href="panier" class="flex items-end justify-center text-center mx-auto pt-2 w-full text-gray-400 group-hover:text-gray-600">
+          <span class="block px-1 pt-1 pb-1">
+            <Cart class="text-2xl pt-1 mb-1 block"/>
+
+            <!-- <span class="block text-xs pb-2">Panier</span> -->
+            <span class="block w-5 mx-auto h-1 group-hover:bg-yellow-500 rounded-full"></span>
+          </span>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <style>
-  @import url(https://pro.fontawesome.com/releases/v5.10.0/css/all.css);
 </style>
 
 <script>
-import Cart from "./icons/cart.vue"
 import Home from "./icons/home.vue"
+import About from "./icons/about.vue"
+import Biscuit from "./icons/biscuit.vue"
+import Cart from "./icons/cart.vue"
 
 export default {
     components: {
+      Home,
+      About,
+      Biscuit,
       Cart,
-      Home 
     }
 }
 </script>
