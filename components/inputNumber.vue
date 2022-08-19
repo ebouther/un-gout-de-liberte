0d67dcc94dbfe871af5f9cfb8384b174a6108b0e
ebouther <ebouther@student.42.fr>
2022-08-19 19:19:04 +0200

    fix: cart & stripe order

--- components/inputNumber.vue
+++ components/inputNumber.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="custom-number-input h-10 w-32">
-    <!-- <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-      <button v-on:click.prevent="decrement" v-bind:disabled="value<=1" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+    <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent">
+      <button v-on:click.prevent="decrement" v-bind:disabled="value<=0" class=" bg-gray-100 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
         <span class="m-auto text-2xl font-thin">−</span>
       </button>
-      <input class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 " name="custom-input-number" v-model.number="input"></input>
-      <button v-on:click.prevent="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+      <input class="outline-none focus:outline-none text-center w-full font-semibold text-xl hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 " name="custom-input-number" v-model.number="input"/>
+      <button v-on:click.prevent="increment" class="bg-gray-100 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
         <span class="m-auto text-2xl font-thin">+</span>
       </button>
-    </div> -->
+    </div>
   </div>
 
 
--- pages/panier.vue
+++ pages/panier.vue
@@ -18,116 +18,158 @@
       <!-- <div v-if="!items || !items.length" class="text-center"><br/><span class="text-gray-700">Le panier est vide.</span></div> -->
       <div class="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
           <div v-for="(i, id) in items" :key="id" class="overflow-hidden flex flex-col border rounded-lg bg-gray-100">
-              <div class="rounded-t-lg bg-white">
-                <img class="object-cover h-48 w-full rounded-t-lg" :src="imgSrc(`${i.dir}/img/small.jpg`)" :alt="i.name">
-              </div>          
-              <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+              <div class="py-4 rounded-lg text-center">
                 <h4 class="mt-1 font-semibold text-base leading-tight truncate text-gray-700">{{i.name}}</h4>
-                <div  class="mt-1 text-sm text-gray-700"><span>{{i.description}}</span></div>
+                <div v-if="i.description" class="mt-1 text-sm text-gray-700"><span>{{i.description}}</span></div>
               </div>
-              <div class="border bg-yellow-100 w-full mt-auto flex justify-between text-sm text-gray-700">
-                <!-- <InputNumber v-bind:value="lookupItemQuantity(id)" v-on:input="q => updateItems(id, q)" /> -->
-                <div class="inline-flex">
-                  <label class=" ml-2 self-center" for="quantity">Quantité:</label>
-                  <input id="quantity" class="mx-2 my-4 w-16 e text-center rounded-md shadow-outline font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 "
-                  name="custom-input-number" v-bind:value="lookupItemQuantity(id)" v-on:input="updateItems(id, $event.target.value)" type="number"/>
-                </div>
+              <div class="bg-white mt-auto">
+                <img class="object-cover h-48 w-full" :src="imgSrc(`${dirname(i._path)}/img/small.jpg`)" :alt="i.name">
+              </div>          
+              <div class="grid grid-cols-3 grid-flow-col">
+                <span class="mx-auto inline-flex items-center text-2xl" >{{(i.price.amount * i.quantity).toFixed(2)}} {{i.price.currency}}</span>
+                <InputNumber class="m-auto border p-0" v-bind:value="i.quantity" v-on:input="q => updateItems(id, q)" />
+                <button v-on:click="removeItem(id)" class="m-2 rounded-md p-3 bg-gray-50">Supprimer</button>
+
+              </div>
+              <!-- <div class="border-t w-full mt-auto flex flex-row justify-between text-sm text-gray-700">
+                <span class="mx-auto inline-flex items-center text-2xl" >{{(i.price.amount * i.quantity).toFixed(2)}} {{i.price.currency}}</span>
+                <InputNumber class="m-auto border p-0" v-bind:value="i.quantity" v-on:input="q => updateItems(id, q)" />
                 <button v-on:click="removeItem(id)" class="m-2 bg-red-600 rounded-md p-3">X</button>
-              </div>
+              </div> -->
               <!-- <InputNumber v-model="i.quantity"/> -->
           </div>
       </div>
     </div>
 </template>
 
-<script>
+<script setup>
 import InputNumber from '~/components/inputNumber'
-// import { mapState, mapGetters } from 'vuex'
+import { useStore } from '~/store/cart'
 
-export default {
-  components: {
-    InputNumber
-  },
-  async asyncData() {
+const cart = useStore()
 
-  },
+const items = cart.items
+console.log('ITEMS : ', items)
 
-  data: () => ({
-    items: [],
-    totalPrice: []
-  }),
+const totalPrice = computed(() => {
+  if (!Object.keys(cart.items).length) return 0;
 
-  //computed: {
-  //  ...mapGetters({}),
-  //  ...mapState({
-  //    items: (state) => state.cart.items,
-  //    totalPrice: (state) => {
-  //      if (!Object.keys(state.cart.items).length) return 0;
+  return Object.values(cart.items)
+          .reduce((acc, i) => acc + i.price.amount * i.quantity, 0)
+          .toFixed(2);
+})
 
-  //      return Object.values(state.cart.items)
-  //              .reduce((acc, i) => acc + i.price.amount * i.quantity, 0)
-  //              .toFixed(2);
-  //    },
-  //  }),
-  //},
+function updateItems (productId, q) {
 
-  mounted() {
+  if (q === '' || Number(q) === NaN) return;
 
-  },
-
-  methods: {
-    lookupItemQuantity (productId) {
-      return this.$store.state.cart.items[productId].quantity
-    },
-    updateItems (productId, q) {
-
-      if (q === '' || Number(q) === NaN) return;
-
-      if (q <= 0) return this.removeItem(productId)
-
-      this.$store.dispatch('cart/updateQuantity', {
-        id: productId,
-        quantity: q 
-      })
-    },
-    removeItem (productId) {
-      this.$store.dispatch('cart/removeItem', productId)
-    },
-    async submit () {
-      //this.$refs.checkoutRef.redirectToCheckout();
-      const res = await this.$axios.$post('/api/order', {
-        items: Object.keys(this.$store.state.cart.items).map(k => ({
-          price: this.$store.state.cart.items[k].price.id,
-          quantity: this.$store.state.cart.items[k].quantity
-        }))
-      });
-
-      console.log('RES : ', res);
-
-      window.location.href = res.url;
-    },
-    imgSrc(src) {
-      const imgs = import.meta.globEager('/content/**/*.{png,jpg}');
-
-      return imgs[`/content${src}`].default
-    },
-  },
-  head() {
-    const title = `Mon Panier`;
-    return {
-      title,
-      meta: [{
-        hid: 'title',
-        name: 'title',
-        content: `${title} - Pâtisseries & Biscuits - Un Goût de Liberté`,
-      }, {
-        hid: 'description',
-        name: 'description',
-        content: `Mon Panier - Un Goût de Liberté - Vente en Ligne de Pâtisseries et Biscuits artisanaux faits à CHILHAC (43380) par Justine DUMESNIL`,
-      }]
-    }
+  if (q <= 0) {
+    return this.removeItem(productId)
   }
+
+  cart.updateQuantity({
+    id: productId,
+    quantity: q 
+  })
 }
+function removeItem (productId) {
+  cart.removeItem(productId)
+}
+
+function imgSrc(src) {
+  const imgs = import.meta.globEager('/content/**/*.{png,jpg}');
+
+  return imgs[`/content${src}`].default
+}
+function dirname(p) {
+  // return path.dirname(p)
+  return p.substr(0, p.lastIndexOf("/"));
+}
+async function submit() {
+  const res = await $fetch('/api/order', {
+    method: 'POST',
+    body: {
+      items: Object.keys(cart.items).map(k => ({
+        price: cart.items[k].price.id,
+        quantity: cart.items[k].quantity
+      }))
+    }
+  });
+
+  console.log('RES : ', res);
+
+  window.location.href = res.url;
+}
+
+//export default {
+//  components: {
+//    InputNumber
+//  },
+//  async asyncData() {
+//
+//  },
+//
+//  data: () => ({
+//    items: [],
+//    totalPrice: []
+//  }),
+//
+//  //computed: {
+//  //  ...mapGetters({}),
+//  //  ...mapState({
+//  //    items: (state) => state.cart.items,
+//  //    totalPrice: (state) => {
+//  //      if (!Object.keys(state.cart.items).length) return 0;
+//
+//  //      return Object.values(state.cart.items)
+//  //              .reduce((acc, i) => acc + i.price.amount * i.quantity, 0)
+//  //              .toFixed(2);
+//  //    },
+//  //  }),
+//  //},
+//
+//  mounted() {
+//
+//  },
+//
+//  methods: {
+
+
+//    async submit () {
+//      //this.$refs.checkoutRef.redirectToCheckout();
+//      const res = await this.$axios.$post('/api/order', {
+//        items: Object.keys(this.$store.state.cart.items).map(k => ({
+//          price: this.$store.state.cart.items[k].price.id,
+//          quantity: this.$store.state.cart.items[k].quantity
+//        }))
+//      });
+//
+//      console.log('RES : ', res);
+//
+//      window.location.href = res.url;
+//    },
+//    imgSrc(src) {
+//      const imgs = import.meta.globEager('/content/**/*.{png,jpg}');
+//
+//      return imgs[`/content${src}`].default
+//    },
+//  },
+//  head() {
+//    const title = `Mon Panier`;
+//    return {
+//      title,
+//      meta: [{
+//        hid: 'title',
+//        name: 'title',
+//        content: `${title} - Pâtisseries & Biscuits - Un Goût de Liberté`,
+//      }, {
+//        hid: 'description',
+//        name: 'description',
+//        content: `Mon Panier - Un Goût de Liberté - Vente en Ligne de Pâtisseries et Biscuits artisanaux faits à CHILHAC (43380) par Justine DUMESNIL`,
+//      }]
+//    }
+//  }
+//}
 </script>
 
 <style>
