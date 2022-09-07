4e5f574336c2472210b7335109c4e7820aacbe6e
ebouther <ebouther@student.42.fr>
2022-09-07 22:50:48 +0200

    chore: rm unused assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,6 @@
 <template>
   <div class="text-center">
     <cart/>
-    <!-- <div class="hidden md:block font-bold text-5xl font-mono text-gray-700">
-      <h1><span class="text-yellow-500"> Un Goût</span> <span class="text-gray-800">de  Liberté</span> </h1>
-    </div>
-    <div class="md:grid md:grid-cols-2 items-center md:pt-14 lg: xl:space-x-10 xl:mx-48">
-      <div class="mx-auto self-center px-3 max-w-md">
-        <nuxt-img class="rounded-lg border-gray-700 border-4" src="logo.jpg" alt="logo">
-      </div>
-      <div class="p-4 self-center">
-        <div class="text-3xl font-light font-mono text-gray-700">
-          <span class="hidden md:block"> Justine Dumesnil</span>
-        </div>
-        <br/>
-        <br/>
-        <div class="text-yellow-500 font-semibold text-2xl text-grey-800 font-mono">
-          <h2>
-            <span class="">Pâtisserie</span>
-            <br/>
-            <span class="md:whitespace-no-wrap">Biscuiterie artisanale</span>
-          </h2>
-        </div>
-        <br />
-        <br />
-        <div class=" text-gray-800 text-xl font-semibold ">
-          <h3>
-          <span class="">Rue des Remparts</span>
-          <br />
-          <span>43380 CHILHAC</span>
-          </h3>
-        </div>
-        <br />
-        <br />
-        <div class="text-gray-800 font-mono text-lg font-light">
-          <span class="">Pâtisserie sur commande au gré des saisons et de vos envies</span>
-        </div>
-      </div>
-    </div>
-    <br /> -->
     <Carousel/>
     <br/>
     <br/>
